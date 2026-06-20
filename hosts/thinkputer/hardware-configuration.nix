48749ad55a5f5ff157e8ca0b8b5835fee9f5cc7e
boris <forum.boris@proton.me>
2026-06-20 17:05:33 -0600

    working on hibernation for laptop -- added a line that likely needs removing

--- hosts/thinkputer/hardware-configuration.nix
+++ hosts/thinkputer/hardware-configuration.nix
@@ -13,6 +13,8 @@
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
+  swapDevices = [ ];
+
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
