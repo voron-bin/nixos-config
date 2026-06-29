eeea1e48fc1d913ce6f5d6310215962388fc2f15
voron-bin <voron.git@pm.me>
2026-06-28 21:17:19 -0600

    upd

--- modules/kernel.nix
+++ modules/kernel.nix
@@ -1,8 +1,8 @@
 # modules-system/kernel.nix
 { host, pkgs, ... }: {
   boot.kernelPackages = 
-    if host.gpuDriver == "discrete" then 
-      pkgs.linuxPackages_zen    # Desktop: Gaming kernel
-    else 
+#    if host.gpuDriver == "discrete" then 
+#      pkgs.linuxPackages_zen    # Desktop: Gaming kernel
+#    else 
       pkgs.linuxPackages;       # Laptop: Default kernel
 }
