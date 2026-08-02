c1815d812cb8d829a5f0b5e55cb383a253c62651
voron-bin <voron.git@pm.me>
2026-08-02 09:08:16 -0600

    upd + priting

--- modules/net.nix
+++ modules/net.nix
@@ -18,5 +18,18 @@
 			DNSOverTLS = "opportunistic";
   		};
 	};
+	
+	services.avahi = {
+		enable = true;
+		nssmdns4 = true;
+		openFirewall = true;
+	};
 
+	services.printing = {
+		enable = true;
+		drivers = with pkgs; [
+			cups-filters
+			cups-browsed
+		];
+	};
 }
